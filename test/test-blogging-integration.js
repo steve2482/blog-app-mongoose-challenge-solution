c90d212f51213306bd66fa16d746aec64c116f6a
steve2482 <stevecb12684@yahoo.com>
2017-01-14 18:36:53 -0500

    added code to generate a post, insert data into the db, and to delete the db

--- test/test-blogging-integration.js
+++ test/test-blogging-integration.js
@@ -10,3 +10,31 @@ const {app, runServer, closeServer} = require('./server');
 const TEST_DATABASE_URL = require('./config');
 
 chai.use(chaiHttp);
+
+// create a function to seed data
+function seedPostData() {
+  const seedData = [];
+  for (let i = 1; i <= 10; i++) {
+    seedData.push(generatePost());
+  }
+  return BlogPost.insertMany(seedData);
+}
+
+// create a function that creates a random post
+function generatePost() {
+  return {
+    author: {
+      firstName: faker.Name.firstName,
+      lastName: faker.Name.lastName
+    },
+    title: faker.Lorem.words,
+    content: faker.Lorem.paragraph,
+    created: Date.now
+  };
+}
+
+// create a function that removes the db
+function removePostData() {
+  console.warn('Deleting Database');
+  return mongoose.connection.dropDatabase();
+}
